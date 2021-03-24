73fd87317ba89efc2d02357f19a50023ce548d4d
Dingdong95 <78545441+Dingdong95@users.noreply.github.com>
2021-03-25 02:11:16 +0900

    chrome-clock-2021.2
    Added
    1. todo list add and save list
    2. delete list and save
    3. random background landscape image

--- todo.js
+++ todo.js
@@ -4,15 +4,44 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+let toDos = [];
+
+
+function deleteToDo(event) {
+    const btn = event.target;                                                     //btn에 event가 발생하면 target해줌
+    const li = btn.parentNode;                                                   //li는 btn의 parentNode임 (id가 parentNode인 list에 추가되기때문)
+    toDoList.removeChild(li);                                                    //toDoList의 child인 event가 발생한 li를 지움
+    const cleanToDos = toDos.filter(function(toDo){                              //toDos array의 element를 filter함 
+                                                                                 //그중 toDo
+        return toDo.id !== parseInt(li.id)
+    });
+    toDos = cleanToDos;                                                          //toDos
+    saveToDos(); 
+}
+
+function saveToDos () {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+
 function paintToDo(text){
-    const li= document.createElement("li");
-    const delBtn = document.createElement("button");
-    delBtn.value = "X";
-    const span = document.createElement("span");
+    const li= document.createElement("li");                           //li를 생성 
+    const delBtn = document.createElement("button");                  //delete button을 생성
+    const span = document.createElement("span");                      //span을 생성 
+    const newId = toDos.length+1;                                     // newID는 toDos array의 길이 +1 임
+    delBtn.innerText = "✔";                                          //delete button의 innertext에 이모지 추가 
+    delBtn.addEventListener("click", deleteToDo);                    //delete버튼에 click event를 감지하고 deleteToDo함수 실행 
     span.innerText = text;
-    li.appendChild(span);
     li.appendChild(delBtn);
+    li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event){
@@ -22,15 +51,19 @@ function handleSubmit(event){
     toDoInput.value = "";
 }
 
-function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if (toDos !== null) {
+function loadToDos() {                                            //loadToDos 함수
+    const loadedToDos = localStorage.getItem(TODOS_LS);           //loadedToDos는 localStorage에 ToDOS_LS를 가져옴 
+    if (loadedToDos !== null) {                                   //loadedToDos 가 null이 아니라면 
+        const parsedToDos = JSON.parse(loadedToDos);              //parsedToDos는 loadedToDos를 parsing하는 함수임. 
+       parsedToDos.forEach(function(toDo){                        //parsedToDos는 parsing된 TODOS_LS array의 element하나마다 toDo에 관하여 
+           paintToDo(toDo.text);                                  //paintToDo를 실행 
+       });
     }
 }
 
-function init() {
-    loadToDos();
-    toDoForm.addEventListener("submit", handleSubmit);
+function init() {                                                  //init은 loadToDos함수와 toDoForm.addEventListener로 이루어짐 
+    loadToDos();                                                   // loadToDos함수를 실행 
+    toDoForm.addEventListener("submit", handleSubmit);             // toDoform.addEventListener함수를 실행 
 }
 
-init();
+init();                                                            //init함수를 실행 
